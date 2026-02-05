6c99aaddbaece16d164cb7f40b9ae9004a38797f
boris <bgrebennikovv@gmail.com>
2026-02-05 16:13:57 +0400

    upd: use LinkedList for store

--- src/main/java/org/skypro/skyshop/search/SearchEngine.java
+++ src/main/java/org/skypro/skyshop/search/SearchEngine.java
@@ -3,28 +3,22 @@ package org.skypro.skyshop.search;
 import org.skypro.skyshop.exceptions.BestResultNotFound;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SearchEngine {
 
-    private final Searchable[] searchItems;
+    private final LinkedList<Searchable> searchItems;
 
-    public SearchEngine(int count) {
-        this.searchItems = new Searchable[count];
+    public SearchEngine() {
+        this.searchItems = new LinkedList<>();
     }
 
     public void add(Searchable searchItem) {
         if (searchItem == null) {
             return;
         }
-
-        for (int i = 0; i < searchItems.length; i++) {
-            if (searchItems[i] == null) {
-                searchItems[i] = searchItem;
-                return;
-            }
-        }
-
+        searchItems.add(searchItem);
     }
 
     private int countOccurrences(String text, String search) {
@@ -50,51 +44,39 @@ public class SearchEngine {
         int maxCount = 0;
 
         for (Searchable item : searchItems) {
+            if (item == null) continue; // на всякий
             String term = item.getSearchTerm();
-            if (term == null) {
-                continue;
-            }
+            if (term == null) continue;
 
             int count = countOccurrences(term, search);
             if (count > maxCount) {
                 maxCount = count;
                 bestMatch = item;
             }
-
         }
 
         if (bestMatch == null) {
             throw new BestResultNotFound(search);
         }
-
         return bestMatch;
     }
 
 
-    public Searchable[] search(String query) {
-        int limit = 5;
-
+    public List<Searchable> search(String query) {
         if (query == null || query.isEmpty()) {
-            return new Searchable[0];
+            return List.of();
         }
 
-        List<Searchable> result = new ArrayList<>();
-
+        List<Searchable> result = new LinkedList<>();
         for (Searchable item : searchItems) {
-            if (item == null) {
-                continue;
-            }
+            if (item == null) continue;
 
-            if (item.getSearchTerm().contains(query)) {
+            String term = item.getSearchTerm();
+            if (term != null && term.contains(query)) {
                 result.add(item);
-
-                if (result.size() == limit) {
-                    break;
-                }
             }
         }
-
-        return result.toArray(new Searchable[0]);
+        return result;
     }
 
 
