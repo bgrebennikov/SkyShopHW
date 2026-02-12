c293f350b9e669381922c74797115b4fb58a2716
boris <bgrebennikovv@gmail.com>
2026-02-12 22:11:28 +0400

    search demo

--- src/main/java/org/skypro/skyshop/App.java
+++ src/main/java/org/skypro/skyshop/App.java
@@ -11,6 +11,7 @@ import org.skypro.skyshop.search.SearchEngine;
 import org.skypro.skyshop.search.Searchable;
 
 import java.util.List;
+import java.util.TreeMap;
 
 public class App {
 
@@ -18,44 +19,12 @@ public class App {
 
         SearchEngine searchEngine = buildSearchEngine();
 
-        try {
-            Searchable result = searchEngine.findBestResult("Banana");
-            System.out.printf("Best result found for \"Banana\": %s%n", result);
-        } catch (BestResultNotFound e) {
-            System.out.printf("Error for \"Banana\": %s%n", e.getMessage());
-        }
+        TreeMap<String, Searchable> searchResults = searchEngine.search("B");
 
-        try {
-            Searchable result2 = searchEngine.findBestResult("Dynamic Island");
-            System.out.printf("Best result found for \"Dynamic Island\": %s%n", result2);
-        } catch (BestResultNotFound e) {
-            System.out.printf("Error for \"Dynamic Island\": %s%n", e.getMessage());
-        }
-
-        try {
-            Searchable result3 = searchEngine.findBestResult("azaza");
-            System.out.printf("Best result found for \"azaza\": %s%n", result3);
-        } catch (BestResultNotFound e) {
-            System.out.printf("Error for \"azaza\": %s%n", e.getMessage());
-        }
-
-        try {
-            Searchable result4 = searchEngine.findBestResult("");
-            System.out.printf("Best result found for empty string: %s%n", result4);
-        } catch (IllegalArgumentException e) {
-            System.out.printf("Input Error for empty string: %s%n", e.getMessage());
-        } catch (BestResultNotFound e) {
-            System.out.printf("Error for empty string: %s%n", e.getMessage());
-        }
-
-        System.out.println("\n=== Search (List results) ===");
-        List<Searchable> searchResults = searchEngine.search("banana");
-        for (Searchable item : searchResults) {
+        for (Searchable item : searchResults.values()) {
             System.out.println(item);
         }
 
-        System.out.println("\n=== Basket removeProductsByName ===");
-        demoBasketRemoveByName();
     }
 
     private static SearchEngine buildSearchEngine() {
@@ -63,7 +32,7 @@ public class App {
         FixedPriceProduct coconut = new FixedPriceProduct("Coconut");
         DiscountedProduct iphone = new DiscountedProduct("Iphone 15", 78000, 15);
 
-        Article article = new Article("banana and coconut smoothie recipe", "Here are a few ways to make a ...");
+        Article article = new Article("Banana and coconut smoothie recipe", "Here are a few ways to make a ...");
         Article article2 = new Article("Iphone 15 Review", "featuring a 48MP main camera, USB-C connectivity, and the Dynamic Island...");
 
         SearchEngine searchEngine = new SearchEngine();
