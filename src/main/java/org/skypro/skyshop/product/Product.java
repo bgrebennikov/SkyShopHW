1d397587aa53c0c29edd1424510f2dc2d9cf2d3d
boris <bgrebennikovv@gmail.com>
2026-02-22 19:07:59 +0400

    override equals & hashcode

--- src/main/java/org/skypro/skyshop/product/Product.java
+++ src/main/java/org/skypro/skyshop/product/Product.java
@@ -3,6 +3,8 @@ package org.skypro.skyshop.product;
 import org.skypro.skyshop.common.ContentType;
 import org.skypro.skyshop.search.Searchable;
 
+import java.util.Objects;
+
 public abstract class Product implements Searchable {
     private final String title;
 
@@ -36,5 +38,22 @@ public abstract class Product implements Searchable {
     @Override
     public String getContentType() {
         return ContentType.PRODUCT.name();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Product product = (Product) obj;
+        return Objects.equals(title, product.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
     }
 }
